3ac4f5d31dde7c66002ddb973224f85ed73b911d
Ayush Kamboj <aayushkamboj7@gmail.com>
2024-03-18 02:10:52 +0530

    Included the JWT token in the successful user login response

--- routes/organisation.js
+++ routes/organisation.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 const Organisation = require("../models/organisation");
 const { createHmac } = require("node:crypto");
+const { createTokenForUser } = require("../utils/jwtUtils");
 
 // Sign Up route for the user
 router.post("/signup", async function (req, res, next) {
@@ -26,9 +27,9 @@ router.post("/signup", async function (req, res, next) {
     contact,
   })
     .then((response) => {
-      res.status(200).json({
-        message: "User created successfully",
-      });
+      res
+        .status(200)
+        .json({ success: true, message: "User created successfully" });
     })
     .catch((error) => {
       res.send(error);
@@ -49,9 +50,18 @@ router.post("/login", function (req, res) {
         .update(password)
         .digest("hex");
 
-      hashedPassword === user.password
-        ? res.status(200).json({ message: "Password Match" })
-        : res.status(400).json({ message: "Password Incorrect" });
+      if (hashedPassword === user.password) {
+        const jwtToken = createTokenForUser(user);
+        res
+          .status(200)
+          .json({ success: true, message: "Password Match", token: jwtToken });
+      } else {
+        res.status(400).json({ success: false, message: "Password Incorrect" });
+      }
+
+      // hashedPassword === user.password
+      //   ?
+      //   : res.status(400).json({ message: "Password Incorrect" });
     }
   });
 });
