b4d12941b93142cdbeb297fb19df1e2cf47d8b5d
Ayush Kamboj <aayushkamboj7@gmail.com>
2024-03-18 01:24:15 +0530

    Login and Sign Up routes logic added in the project

--- routes/organisation.js
+++ routes/organisation.js
@@ -1,32 +1,59 @@
 var express = require("express");
 var router = express.Router();
 const Organisation = require("../models/organisation");
-const { response } = require("../app");
+const { createHmac } = require("node:crypto");
 
-router.get("/", (req, res) => {
-  res.send("Hi this is from the org routes");
+// Sign Up route for the user
+router.post("/signup", async function (req, res, next) {
+  const {
+    name,
+    description,
+    email,
+    password,
+    profileImageURL,
+    role,
+    location,
+    contact,
+  } = req.body;
+  await Organisation.create({
+    name,
+    description,
+    email,
+    password,
+    profileImageURL,
+    role,
+    location,
+    contact,
+  })
+    .then((response) => {
+      res.status(200).json({
+        message: "User created successfully",
+      });
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 });
 
-router.post("/", async function (req, res, next) {
-  Organisation.matchPassword("banyz12abyus@gmai.com").then((response) =>
-    res.send(response.password)
-  );
-  // await Organisation.create({
-  //   name: "XYZ PVT LTD",
-  //   description: "Hi this is the description",
-  //   email: "  sasddbanyz12abyus@gmai.com",
-  //   password: "123456798",
-  //   profileImageURL: "fdhbhdsfhds",
-  //   role: "Vendor",
-  //   location: "shdbfhsbf",
-  //   contact: 1234567498,
-  // })
-  //   .then((response) => {
-  //     res.send(response);
-  //   })
-  //   .catch((error) => {
-  //     res.send(error);
-  //   });
+// Login Route for User
+router.post("/login", function (req, res) {
+  const { email, password } = req.body;
+  Organisation.findOne({ email }).then((user) => {
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+    } else {
+      const hashedPassword = createHmac(
+        "sha256",
+        "this is the salt for hashing"
+      )
+        .update(password)
+        .digest("hex");
+
+      hashedPassword === user.password
+        ? res.status(200).json({ message: "Password Match" })
+        : res.status(400).json({ message: "Password Incorrect" });
+    }
+  });
 });
 
 module.exports = router;
