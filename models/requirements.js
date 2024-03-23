5cc58cc8066c4e546ff4936770c9c8e77ee032d3
Ayush Kamboj <aayushkamboj7@gmail.com>
2024-03-23 11:29:47 +0530

    Logic to store and reterive requests are changed

--- models/requirements.js
+++ models/requirements.js
@@ -1,27 +1,37 @@
 const { Schema, model } = require("mongoose");
 
-const requirementSchema = new Schema({
-  product: {
-    type: String,
-    require: true,
+const requirementSchema = new Schema([
+  {
+    email: {
+      type: String,
+      require: true,
+    },
+    product: {
+      type: String,
+      require: true,
+    },
+    product_desc: {
+      type: String,
+      require: true,
+    },
+    budget_min: {
+      type: Number,
+      require: true,
+    },
+    budget_max: {
+      type: Number,
+      require: true,
+    },
+    isAccepted: {
+      type: Boolean,
+      require: true,
+    },
+    sector: {
+      type: String,
+      require: true,
+    },
   },
-  product_desc: {
-    type: String,
-    require: true,
-  },
-  budget_min: {
-    type: Number,
-    require: true,
-  },
-  budget_max: {
-    type: Number,
-    require: true,
-  },
-  isAccepted: {
-    type: Boolean,
-    require: true,
-  },
-});
+]);
 
 const Requirements = model("requirement", requirementSchema);
 
--- routes/requirements.js
+++ routes/requirements.js
@@ -1,39 +1,42 @@
 var express = require("express");
 const Organisation = require("../models/organisation");
+const { Requirements } = require("../models/requirements");
 var router = express.Router();
 
 // To the list of requirements
 router.get("/:email", async (req, res) => {
   const email = req.params.email;
-  await Organisation.findOne({ email })
-    .then((response) => res.status(200).json({ success: true, response }))
-    .catch((error) =>
-      res.status(400).json({ success: false, message: "Error happened", error })
-    );
+  await Requirements.find({ email }).then((response) => res.send(response));
+  //   await Organisation.findOne({ email })
+  //     .then((response) => res.status(200).json({ success: true, response }))
+  //     .catch((error) =>
+  //       res.status(400).json({ success: false, message: "Error happened", error })
+  //     );
 });
 
 router.post("/post", async (req, res) => {
-  const { email, product, product_desc, budget_min, budget_max, isAccepted } =
-    req.body;
+  const {
+    email,
+    product,
+    product_desc,
+    budget_min,
+    budget_max,
+    isAccepted,
+    sector,
+  } = req.body;
 
-  let requirement = await Organisation.findOneAndUpdate(
-    { email },
-    {
-      $push: {
-        requirements: {
-          product,
-          product_desc,
-          budget_min,
-          budget_max,
-          isAccepted,
-        },
-      },
-    },
-    { new: true }
-  );
+  let requirement = await Requirements.create({
+    email,
+    product,
+    product_desc,
+    budget_min,
+    budget_max,
+    isAccepted,
+    sector,
+  });
   res.send(requirement);
 });
 
-router.post("/accept", async (req, res) => {});
+// router.post("/accept", async (req, res) => {});
 
 module.exports = router;
