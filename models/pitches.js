9bce17ea7a8bc116c29e40b0fec6bb915599bfd4
Ayush Kamboj <aayushkamboj7@gmail.com>
2024-05-17 01:27:26 +0530

    Pitches model added in the project

---
+++ models/pitches.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const pitchesSchema = new Schema({
+  company_name: {
+    type: String,
+    required: true,
+  },
+  company_email: {
+    type: String,
+    required: true,
+  },
+  place: {
+    type: String,
+    required: true,
+  },
+  budget_min: {
+    type: Number,
+    required: true,
+  },
+  budget_max: {
+    type: Number,
+    required: true,
+  },
+  product_details: {
+    type: String,
+    required: true,
+  },
+});
+
+const Pitches = model("pitches", pitchesSchema);
+
+module.exports = { Pitches, pitchesSchema };
