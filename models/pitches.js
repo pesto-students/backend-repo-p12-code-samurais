39c8952b086b7441effd9c6ed84a2999fe3e0095
Ayush Kamboj <aayushkamboj7@gmail.com>
2024-05-18 01:02:16 +0530

    Time stamp added in the models

--- models/pitches.js
+++ models/pitches.js
@@ -1,32 +1,35 @@
 const { Schema, model } = require("mongoose");
 
-const pitchesSchema = new Schema({
-  company_name: {
-    type: String,
-    required: true,
+const pitchesSchema = new Schema(
+  {
+    company_name: {
+      type: String,
+      required: true,
+    },
+    company_email: {
+      type: String,
+      required: true,
+    },
+    place: {
+      type: String,
+      required: true,
+    },
+    budget_min: {
+      type: Number,
+      required: true,
+    },
+    budget_max: {
+      type: Number,
+      required: true,
+    },
+    product_details: {
+      type: String,
+      required: true,
+    },
+    requirement_id: { type: Schema.Types.ObjectId, ref: "Requirement" },
   },
-  company_email: {
-    type: String,
-    required: true,
-  },
-  place: {
-    type: String,
-    required: true,
-  },
-  budget_min: {
-    type: Number,
-    required: true,
-  },
-  budget_max: {
-    type: Number,
-    required: true,
-  },
-  product_details: {
-    type: String,
-    required: true,
-  },
-  requirement_id: { type: Schema.Types.ObjectId, ref: "Requirement" },
-});
+  { timestamps: true }
+);
 
 const Pitches = model("pitches", pitchesSchema);
 
--- models/requirements.js
+++ models/requirements.js
@@ -1,34 +1,37 @@
 const { Schema, model } = require("mongoose");
 
-const requirementSchema = new Schema({
-  product: {
-    type: String,
-    required: true,
+const requirementSchema = new Schema(
+  {
+    product: {
+      type: String,
+      required: true,
+    },
+    product_desc: {
+      type: String,
+      required: true,
+    },
+    budget_min: {
+      type: Number,
+      required: true,
+    },
+    budget_max: {
+      type: Number,
+      required: true,
+    },
+    req_sector: {
+      type: String,
+      required: true,
+      enum: ["IT", "Automobile"],
+    },
+    organisation_id: { type: Schema.Types.ObjectId, ref: "Organisation" },
+    organisation_email: {
+      type: String,
+      required: true,
+    },
+    pitches: [{ type: { isPitchAccepted: Boolean } }],
   },
-  product_desc: {
-    type: String,
-    required: true,
-  },
-  budget_min: {
-    type: Number,
-    required: true,
-  },
-  budget_max: {
-    type: Number,
-    required: true,
-  },
-  req_sector: {
-    type: String,
-    required: true,
-    enum: ["IT", "Automobile"],
-  },
-  organisation_id: { type: Schema.Types.ObjectId, ref: "Organisation" },
-  organisation_email: {
-    type: String,
-    required: true,
-  },
-  pitches: [{ type: { isPitchAccepted: Boolean } }],
-});
+  { timestamps: true }
+);
 
 const Requirements = model("requirement", requirementSchema);
 
