b5ecadf23df9f0e2c2b3b48a6ec0939269a3561b
Ayush Kamboj <aayushkamboj7@gmail.com>
2024-06-04 00:57:44 +0530

    Route to get chats and send messages added

--- routes/chat.js
+++ routes/chat.js
@@ -1,9 +1,18 @@
 var express = require("express");
 var router = express.Router();
 const publisher = require("../chatServer/publisher");
+const subscriber = require("../chatServer/subscriber");
 
+// Route to get the messages from a queue
 router.get("/", async (req, res) => {
-  await publisher();
+  subscriber("ayush@gmail.com", "dency@gmail.com");
+  res.send("Messages Recieved");
+});
+
+// Route 
+router.post("/send_message", async (req, res) => {
+  const { sender_email, receiver_email, message } = req.body;
+  await publisher(sender_email, receiver_email, message);
   res.send("Message Sent");
 });
 
